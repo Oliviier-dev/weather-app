defa8f4c33724c3ec31e73d7917cb4a1c0f07b7c
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-09-08 22:05:21 +0200

    Done initial initialisation

---
+++ script.js
@@ -0,0 +1,5 @@
+let cityDesc = document.querySelector('.city_desc');
+let degrees = document.querySelector('.degrees');
+let cloudStatus = document.querySelector('.description');
+let humidity = document.querySelector('.humidity');
+let wind = document.querySelector('.wind');
