1ee264276d4a8427785db9ef087c305909928043
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-09-10 14:30:49 +0200

    Created a function to call weather data when the search is clicked

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const api = 'a14186e30bb8471686763140230609';
 let cityDesc = document.querySelector('.city_desc');
+let searchBtn = document.querySelector('.btn');
+let searchInput = dcument.querySelector('.search-input');
 let degrees = document.querySelector('.degrees');
 let cloudStatus = document.querySelector('.description');
 let humidity = document.querySelector('.humidity');
 let wind = document.querySelector('.wind');
+
+searchBtn.addEventListener('click', () =>{
+    const city = searchInput.value;
+
+    if (city){
+        getWeatherData(city);
+    }
+})
+
+function getWeatherData(city){
+
+}
